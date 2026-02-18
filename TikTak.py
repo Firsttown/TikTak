ff15ff5dddbcf6525611c3b417d90cc248963088
RadioGlitch <57309672+Radi0Glitch@users.noreply.github.com>
2026-02-18 10:02:37 +0700

    Update TikTak.py

--- TikTak.py
+++ TikTak.py
@@ -44,7 +44,7 @@ current_state = STATE_AUTH
 user_data = {
     "telegram_username": "",
     "chat_id": None,
-    "bot_token": "6276440729:AAFxvHeRXUsUmf4RHpthKU_u_hFpxXczeho",  #@RadGL_bot для начала работы нужно будет отправить боту /start
+    "bot_token": "YOUR_TOKEN",  
     "game_stats": {"wins": 0, "losses": 0, "draws": 0}
 }
 
